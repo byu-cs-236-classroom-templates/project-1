fa1e45a8a29f53a34084d812e562f29bf0670a61
Eric Mercer <egm@cs.byu.edu>
2024-09-04 23:04:56 +0000

    Add docstrings to lexer

--- src/project1/lexer.py
+++ src/project1/lexer.py
@@ -1,3 +1,20 @@
+"""Turn a input string into a stream of tokens with lexical analysis.
+
+The `lexer(input_string: str)` function is the entry point. It generates a
+stream of tokens from the `input_string`.
+
+Typical usage example:
+
+    >>> from project1.lexer import lexer
+    >>> input_string = ":\\n  \\n:"
+    >>> for i in lexer(input_string):
+    ...     print(i)
+    ...
+    (COLON,":",1)
+    (COLON,":",3)
+    (EOF,"",3)
+"""
+
 from typing import Iterator
 
 from project1.token import Token, TokenType
@@ -34,8 +51,40 @@ def _lexer(
 
 
 def lexer(input_string: str) -> Iterator[Token]:
+    """Produce a stream of tokens from a given input string.
+
+    Pseudo-code:
+
+    ```
+    fsms: list[FiniteStateMachine] = [Colon(), Eof(), WhiteSpace()]
+    hidden: list[TokenType] = ["WHITESPACE"]
+    line_num: int = 1
+    token: Token = Token.undefined("")
+    while token.token_type != "EOF":
+        token = _get_token(input_string, fsms)
+        token.line_num = line_num
+        line_num = line_num + _get_new_lines(token.value)
+        input_string = input_string.removeprefix(token.value)
+        if token.token_type in hidden:
+            continue
+        yield token
+    ```
+
+    The `_get_token` function should return the token from the FSM that reads
+    the most characters. In the case of two FSMs reading the same number of
+    characters, the one that comes first in the list of FSMs, `fsms`, wins.
+
+    Args:
+
+        input_string (str): input string for token generation
+
+    Yields:
+
+        token (Token): the current token resulting from the string
+    """
     fsms: list[FiniteStateMachine] = [Colon(), Eof(), WhiteSpace()]
     hidden: list[TokenType] = ["WHITESPACE"]
 
+    # raise NotImplementedError
     for i in _lexer(input_string, fsms, hidden):
         yield i
